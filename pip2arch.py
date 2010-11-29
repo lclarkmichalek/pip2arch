62f9ea5f2fbb0aff787e4d2054801839b676ee66
Wieland Hoffmann <themineo@gmail.com>
2010-11-29 13:52:26 +0100

    Fix the "Written PKGBUILD" message

--- pip2arch.py
+++ pip2arch.py
@@ -153,4 +153,4 @@ if __name__ == '__main__':
         p.add_makedepends(args.makedepends)
     print "Got package information"
     args.outfile.write(p.render())
-    print "Written PKGBUILD"
+    print "Wrote the PKGBUILD"
