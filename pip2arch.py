248a447bc6afe1e53f0264b6dc1aef19c7447f47
Wieland Hoffmann <themineo@gmail.com>
2010-11-29 14:30:32 +0100

    Fix a bug in choose_version
    Not returning what the second (or greater) call of self.choose_version
    returns makes no sense.

--- pip2arch.py
+++ pip2arch.py
@@ -140,7 +140,7 @@ class Package(object):
         else:
             print 'That was NOT one of the choices...'
             print 'Try again'
-            self.choose_version(versions)
+            return self.choose_version(versions)
 
     def add_depends(self, depends):
         self.depends += depends
