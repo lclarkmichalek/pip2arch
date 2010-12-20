f40d9c4b5db32f42dc22ce1263cadb71cb87e619
Wieland Hoffmann <themineo@gmail.com>
2010-12-20 18:58:47 +0100

    Adding an incomplete setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(  name="pip2arch",
+        version="x.x",
+        description="Convert pypi packages to Arch Linux PKGBUILDs",
+        author="Laurie Clark-Michalek",
+        author_email="BluePeppers@archlinux.us",
+        scripts=["pip2arch.py"],
+        download_url=["http://github.com/bluepeppers/pip2arch/tarball/master"],
+        url=["http://github.com/bluepeppers/pip2arch"],
+        classifiers=[
+            "Environment :: Console",
+            "Intended Audience :: End Users/Desktop",
+            "Natural Language :: English",
+            "Operating System :: OS Independent"
+            ]
+        )
